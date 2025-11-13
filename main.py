540dbfa0c0091e9217fcec0e9c050300d90b64da
Yehuda Freiman <yehfre@icloud.com>
2025-11-13 10:00:31 +0200

    Add FastAPI endpoints for various functionalities
    Implemented multiple endpoints for FastAPI including index, testing, name saving, and Caesar and fence cipher functionalities.

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import json
+
+api = FastAPI()
+
+@api.get("/")
+def index():
+    return {"msg":"Welcome to the hottest place in hell"}
+
+@api.get("/test")
+def testing():
+    return {"msg":"Hi from test"}
+
+@api.get("/test/{name}")
+def save_name(name):
+    with open("names.txt", "a") as f:
+        f.write(name)
+    return {"msg":"Saved user"}
+
+@api.post("/caesar")
+def caesar_cipher(offset):
+    body = {"text": str,
+           "offset": int,
+           "mode": "encrypt" or "decrypt"
+    }
+    return {"encrypted_text":""} or {"decrypted_text":""}
+
+@api.get("/fence/encrypt?text={text}")
+def fence_cipher_encryption():
+    return {"encrypted_text":""}
+
+@api.post("/fence/decrypt")
+def fence_cipher_decryption():
+    body = {"text":str
+    }
+    return {"decrypted":""}
+
+
+
 
